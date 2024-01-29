a510c26a84933f9399bc512202dcfc1df8af4928
o7-machienhum <ryan.cjw@gmail.com>
2024-01-29 10:46:51 -0800

    Added flashing script

---
+++ flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    >&2 echo "No arguments provided. Usage: ./flash.sh /dev/sdX"
+    exit 1
+fi
+
+cowsay Flashing SD card! $1
+sudo dd if=buildroot/output/images/sdcard.img of=$1 bs=4M conv=fsync
