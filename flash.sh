e68d5951f3dd434a5304d69adf25bd6c21262a59
o7-machienhum <ryan.cjw@gmail.com>
2024-02-21 12:09:45 -0800

    Added some logic to pulloff pcap files.

--- flash.sh
+++ flash.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "" ]; then
     echo "No arguments provided. Usage: ./flash.sh /dev/sdX"
     echo "                              ./flash.sh 192.168.1.100"
+    echo "                              ./flash.sh 192.168.1.100 pull"
     exit 1
 fi
 
@@ -15,9 +16,14 @@ if [[ "$1" == /dev/* ]]; then
 fi
 
 IP=$1
+OP="-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+if [[ "$2" == "pull" ]]; then
+	sshpass -p 'blackhat' scp $OP root@$IP:/root/dump2.pcap . 
+	exit
+fi
 
 cowsay -f ghostbusters Flashing rootfs to $IP
-OP="-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 sshpass -p 'blackhat' scp $OP package/blackhat/src/blackhat.sh root@$IP:/usr/bin/bh 
 sshpass -p 'blackhat' scp $OP package/blackhat/src/blackhat.conf root@$IP:/etc/blackhat.conf
 sshpass -p 'blackhat' scp $OP rootfs_overlay/etc/dnsmasq.conf root@$IP:/etc/dnsmasq.conf
