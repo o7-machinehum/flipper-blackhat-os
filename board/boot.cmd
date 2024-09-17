aaf3d781bb796d28403f0d4c23021c1a0fa6a899
o7-machinehum <ryan.cjw@gmail.com>
2024-09-17 21:15:25 +0200

    Changed kernel command line to use uuid.

--- board/boot.cmd
+++ board/boot.cmd
@@ -1,4 +1,6 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk1p2 rootwait panic=10 ${extra}
+setenv finduuid "part uuid mmc 0:2 uuid"
+run finduuid
+setenv bootargs console=ttyS0,115200 root=PARTUUID=${uuid} rootwait panic=10 ${extra}
 ext4load mmc 0:2 0x49000000 /boot/${fdtfile}
 ext4load mmc 0:2 0x46000000 /boot/zImage
 env set fdt_high ffffffff
