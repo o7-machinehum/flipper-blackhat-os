efcfad418bdb4f8faf01d756f23404736cf42318
o7-machinehum <ryan.cjw@gmail.com>
2024-10-26 17:16:05 +0200

    Quite a bit done on the software.

--- package/blackhat/src/blackhat.sh
+++ package/blackhat/src/blackhat.sh
@@ -16,36 +16,55 @@ else
 	exit
 fi
 
-echo "Loaded Config From: $CONFIG_F"
+echo "Loaded Config: $CONFIG_F"
 
 source $CONFIG_F 
 rm $LOG_F 2>/dev/null
 
 function print_help() {
-	echo "Usage (eg): bh set SSID MySSID"
 	echo "Commands:"
+    echo "useage: bh wifi connect wlan0"
 	echo "  set"
-	echo "    SSID       Set SSID of WiFi network to connect to"
-	echo "    PASS       Set password for WiFi network: SSID"
-	echo "    AP_SSID    Set SSID of WiFi network you're creating"
-	echo "  wifi         Connect to a WiFi network"
-    echo "    list       List WiFi APs"
-    echo "    connect    Connect to WiFi AP"
-	echo "    ap         Enable Access Point"
-	echo "  ssh          Enable SSH"
-	echo "  evil_twin    Enable the evil twin AP"
-	echo "  evil_portal  Enable the evil portal AP"
-    echo "  rat_driver   Enable RAT Driving"
-	echo "  get          Get currently set parameters"
+	echo "    SSID            Set SSID of WiFi network to connect to"
+	echo "    PASS            Set password for WiFi network: SSID"
+	echo "    AP_SSID         Set SSID of WiFi network you're creating"
+	echo "  wifi              Connect to a WiFi network"
+    echo "    list            List WiFi APs"
+    echo "    con <interface> Connect to WiFi AP"
+    echo "    dev             List devices"
+	echo "    ap <interface>  Enable Access Point"
+    echo "    ip              Get IP Address"
+	echo "  ssh               Enable SSH"
+	echo "  evil_twin         Enable the evil twin AP"
+	echo "  evil_portal       Enable the evil portal AP"
+    echo "  rat_driver        Enable RAT Driving"
+	echo "  get               Get currently set parameters"
 }
 
 function connect_wifi() {
+	ip link set $1 up
 	wpa_supplicant -B -i $1 -c <(wpa_passphrase $SSID $PASS)
+}
+
+function start_ap() {
+    ap_nic=$1
+	ip link set $ap_nic down
+	ip addr add 192.168.2.1/24 dev $ap_nic
+
+    sed -i "s/^ssid=.*/ssid=$AP_SSID/" /etc/hostapd.conf
+
+	kill $(pidof hostapd)
+	hostapd /etc/hostapd.conf -i $1 &
+
+	kill $(pidof dnsmasq)
+	dnsmasq -C /etc/dnsmasq.conf -d 2>&1 > $LOG_F & 
+
 }
 
 function evil_twin() {
 	ip link set $RADIO_AP down
-	ip addr add 192.168.99.1/24 dev $RADIO_AP
+	ip addr add 192.168.2.1/24 dev $RADIO_AP
+
 	iptables --table nat --append POSTROUTING --out-interface $RADIO_CLIENT -j MASQUERADE
 	iptables --append FORWARD --in-interface $RADIO_AP -j ACCEPT
 	echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -57,34 +76,42 @@ function evil_twin() {
 	dnsmasq -C /etc/dnsmasq.conf -d 2>&1 > $LOG_F & 
 }
 
-function ap() {
-    echo "Opening Access Point"
-
-}
 
 function evil_portal() {
     echo "Not Implemented yet!"
 }
 
-function evil_portal() {
-    echo "Not Implemented yet!"
-}
-
-function set() {
+function set_param() {
 	sed -i "/^$1=/c$1=\"$2\"" ${CONFIG_F}	
+}
+
+function check() {
+    if [ -z $1 ]; then
+        print_help
+        exit
+    fi
 }
 
 function wifi() {
 	case "$1" in
 		list)
-			iw $RADIO_CLIENT scan | grep "SSID:"
+            check $2            
+			iw $2 scan | grep "SSID:"
 			;;
 		connect)
-			ip link set $RADIO_CLIENT up
-			connect_wifi $RADIO_CLIENT
+            check $2            
+			connect_wifi $2
 			;;
         ap)
-            echo "Not implemented YEt"
+            check $2            
+            start_ap $2
+            ;;
+        dev)
+            iw dev | grep -e phy -e wlan
+            iw list | awk '/Wiphy/{if(phy) {if(b1 && b2) {print "phy" phy ": 2.4 GHz and 5 GHz"} else if(b1) {print "phy" phy ": 2.4 GHz"} else if(b2) {print "phy" phy ": 5 GHz"}; b1=0; b2=0} phy=$2} /Band 1/{b1=1} /Band 2/{b2=1} END{if(b1 && b2){print "phy" phy ": 2.4 GHz and 5 GHz"} else if(b1){print "phy" phy ": 2.4 GHz"} else if(b2){print "phy" phy ": 5 GHz"}}'
+            ;;
+        ip)
+            ip addr | grep wlan | awk -F': <' '{print $1}' | awk -F'/24' '{print $1}'
             ;;
 		*)
 			print_help
@@ -94,7 +121,7 @@ function wifi() {
 subcommand=$1; shift  
 case "$subcommand" in
 	set)
-		set "$@"
+		set_param "$@"
 		;;
 	get)
 		cat $CONFIG_F
@@ -108,6 +135,9 @@ case "$subcommand" in
 	help)
 		print_help
 		;;
+    pull)
+        scp machinehum@192.168.1.103:/home/machinehum/projects/flipper-blackhat-os/package/blackhat/src/blackhat.sh /usr/bin/bh
+        ;;
 	*)
 		print_help
 		;;
