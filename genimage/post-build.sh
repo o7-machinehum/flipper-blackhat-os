aa5c0f6c995eab314eeb6ede915681d18a0a2152
o7-machinehum <ryan.cjw@gmail.com>
2024-10-28 11:15:33 +0100

    Changed to spaces, added blackhat config file to fat32

--- genimage/post-build.sh
+++ genimage/post-build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
 cp $CONFIG_DIR/../genimage/readme.txt $BINARIES_DIR
+cp $CONFIG_DIR/../package/blackhat/src/blackhat.conf $BINARIES_DIR
