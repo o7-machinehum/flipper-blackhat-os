877a3921f425a6d85a6de8b73fff151b56b1fc87
o7-machinehum <ryan.cjw@gmail.com>
2024-10-29 16:27:41 +0100

    Removed a bunch of init.d stuff, added index.html

--- genimage/post-build.sh
+++ genimage/post-build.sh
@@ -2,3 +2,9 @@
 cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
 cp $CONFIG_DIR/../genimage/readme.txt $BINARIES_DIR
 cp $CONFIG_DIR/../package/blackhat/src/blackhat.conf $BINARIES_DIR
+cp $CONFIG_DIR/../package/blackhat/src/index.html $BINARIES_DIR
+rm -f $TARGET_DIR/etc/init.d/S50dropbear
+rm -f $TARGET_DIR/etc/init.d/S50apache
+rm -f $TARGET_DIR/etc/init.d/S35iptables
+rm -f $TARGET_DIR/etc/init.d/S40network
+rm -f $TARGET_DIR/etc/init.d/S80dnsmasq
